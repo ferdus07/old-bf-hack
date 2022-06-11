c036ea663bda02106916c152bda8c65451498ec8
ferdus07 <107324845+ferdus07@users.noreply.github.com>
2022-06-12 03:34:10 +0600

    Create pythonLover.py
    git clone https://github.com/Niki404-Cyber/Green-Lover.git
    ls
     cd Greeen-Lover
    ls
    python2pythonLover.py

---
+++ pythonLover.py
@@ -0,0 +1,36 @@
+cd
+
+rm -rf Green-Lover
+
+ pkg update
+
+ pkg upgrade
+
+ pkg install python
+
+pkg install python2
+
+ pip install requests
+
+ pip install mechanize
+
+ pip install bs4
+
+ pkg install git
+
+ git clone https://github.com/Niki404-Cyber/Green-Lover.git
+
+ cd Greeen-Lover
+
+ python Lover.py
+
+
+
+
+
+
+
+
+
+
+
